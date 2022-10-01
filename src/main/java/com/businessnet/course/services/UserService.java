fff03dcd8c4de9475fca6c835ba22f9d7e37868e
Ivan Carlos <ivancarlos50@gmail.com>
2022-10-01 12:52:58 -0300

    Service layer, component registration

---
+++ src/main/java/com/businessnet/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.businessnet.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.businessnet.course.entities.User;
+import com.businessnet.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj =  repository.findById(id);
+		return obj.get();
+	}
+	
+
+}
